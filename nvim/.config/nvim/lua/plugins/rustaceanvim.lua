5745704dc7f5ca6a35b06e238484897903bad6dc
optizone <ilya.kek.lol.orbidol@gmail.com>
2025-02-28 01:03:24 +0300

    use nightly fmt for rust

--- nvim/.config/nvim/lua/plugins/rustaceanvim.lua
+++ nvim/.config/nvim/lua/plugins/rustaceanvim.lua
@@ -1,6 +1,8 @@
 return {
   "mrcjkb/rustaceanvim",
   opts = function(_, opts)
-    opts.server.default_settings["rust-analyzer"].procMacro.ignored["async-trait"] = nil
+    local ra = opts.server.default_settings["rust-analyzer"]
+    ra.procMacro.ignored["async-trait"] = nil
+    ra.rustfmt = { extraArgs = { "+nightly" } }
   end,
 }
